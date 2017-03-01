6387a0a2dab08d2700164142aef10cf8cea46410
AlejandroHerr <alejandrohnc88@gmail.com>
2017-03-01 20:03:33 +0100

    Skeleton

---
+++ app/app.php
@@ -0,0 +1,24 @@
+<?php
+
+use Stack\Builder;
+use Symfony\Component\HttpFoundation\Request;
+
+$app = require './app/rrdweb.php';
+
+$cors = [
+    'allowedHeaders' => ['x-allowed-header', 'x-other-allowed-header'],
+    'allowedMethods' => ['GET'],
+    'allowedOrigins' => ['*'],
+    'exposedHeaders' => false,
+    'maxAge' => false,
+    'supportsCredentials' => false,
+];
+
+$stack = (new Builder())
+    ->push('Asm89\Stack\Cors', $cors);
+
+$app = $stack->resolve($app);
+
+$request = Request::createFromGlobals();
+$response = $app->handle($request)->send();
+$app->terminate($request, $response);
